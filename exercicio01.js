4bd4d39497fae3634b30428b330ff88b8621d250
LUANA RIBEIRO SANCHES FERNANDES <54679953837@imaccommand020.local>
2025-11-19 13:40:33 -0300

    exercicio1

---
+++ exercicio01.js
@@ -0,0 +1,10 @@
+let username = prompt("Digite seu usuário: ")
+
+let senha = prompt("Digite sua senha")
+
+if(username == "admin" && senha == "senha123"){
+    console.log("Login bem-sucedido")
+}
+else{
+    console.log("Login incorreto! ;-;")
+}
